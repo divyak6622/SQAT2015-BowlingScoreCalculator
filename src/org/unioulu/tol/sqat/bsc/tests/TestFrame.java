695db081f5d7977ae31f500b1794a8522ce73168
somename <someemail>
2015-10-16 11:35:50 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/tests/TestFrame.java
+++ src/org/unioulu/tol/sqat/bsc/tests/TestFrame.java
@@ -3,12 +3,13 @@ package org.unioulu.tol.sqat.bsc.tests;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.unioulu.tol.sqat.bsc.Frame;
 
 public class TestFrame {
 
 	@Test
 	public void testFrameScore() {
-		
+		Frame frame1 = new Frame(0,0);
 	}
 	
 }
