235b6661e9bd765b706dd40f433a8d7e7440a7a4
somename <someemail>
2015-10-16 11:43:39 +0300

    besouro automatic message

---
+++ src/org/unioulu/tol/sqat/bsc/tests/TestBowlingGame.java
@@ -0,0 +1,11 @@
+package org.unioulu.tol.sqat.bsc.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.unioulu.tol.sqat.bsc.Frame;
+
+
+public class TestBowlingGame {
+
+}
