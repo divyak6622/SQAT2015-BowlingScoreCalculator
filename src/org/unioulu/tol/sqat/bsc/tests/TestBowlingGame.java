ba9c35b07166e5b834f42ae1a6adeaa13322d27f
somename <someemail>
2015-10-16 12:54:13 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/tests/TestBowlingGame.java
+++ src/org/unioulu/tol/sqat/bsc/tests/TestBowlingGame.java
@@ -92,5 +92,7 @@ public class TestBowlingGame {
 	public void testGameScoreSingleSpareSecondLast() {
 		ArrayList<Frame> gameFrames = new ArrayList<Frame>(Arrays.asList(frames));
 		gameFrames.set(8, new Frame(9,1));
+		
+		int expectedScore = 84;
 	}
 }
